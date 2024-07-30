14b0b6fa3f75f8d6719f22296ebafa8648ce51ed
Aloïs Kouadio <aloiskouadio@gmail.com>
2024-07-30 08:50:17 +0000

    mise en place des effets d'hover sur le form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InputFormComponent } from './input-form/input-form.component';
+import { InputElement1Component } from './input-form/input-element-1/input-element-1.component';
+import { InputElement2Component } from './input-form/input-element-2/input-element-2.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports : [
-    InputFormComponent
+    InputFormComponent,
+    InputElement1Component,
+    InputElement2Component
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
