ae51dbee56b2561e1fce25b4a038645f4b003556
Aloïs Kouadio <aloiskouadio@gmail.com>
2024-07-30 14:25:16 +0000

    mise en place de la partie results

---
+++ src/app/resultat/resultat-text/resultat-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultat-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './resultat-text.component.html',
+  styleUrl: './resultat-text.component.css'
+})
+export class ResultatTextComponent {
+
+}
