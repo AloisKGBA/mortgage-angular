c266a8ae11dc7c0c51f1eca400c5ae90a2cdb6e9
Aloïs Kouadio <aloiskouadio@gmail.com>
2024-08-01 17:16:50 +0000

    mise à jour de la page à vide et mise en place du bloc résultas

---
+++ src/app/resultat/resultat-show/show-calcul/show-calcul.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-calcul',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-calcul.component.html',
+  styleUrl: './show-calcul.component.css'
+})
+export class ShowCalculComponent {
+
+}
