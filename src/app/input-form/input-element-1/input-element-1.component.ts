2fac450781be538156c234c763729cf10fcd0b4c
Aloïs Kouadio <aloiskouadio@gmail.com>
2024-08-02 15:42:37 +0000

    finalisation de la deuxième partie des résultats

--- src/app/input-form/input-element-1/input-element-1.component.ts
+++ src/app/input-form/input-element-1/input-element-1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-element-1',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './input-element-1.component.html',
   styleUrl: './input-element-1.component.css'
 })
-export class InputElement1Component {
+export class InputElement1Component implements OnInit {
+  constructor() {
+  }
 
+  ngOnInit(): void {
+      this.checkamount();
+  }
+
+  checkamount(): void {
+    let amount = document.getElementById("Amount") as HTMLInputElement;
+    if (amount.value === "") {
+      alert("Cest zooooooooooooooo");
+    }
+  }
 }
